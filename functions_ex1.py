0c3aa30b4cbfbf15b26bf74f4f6bc073c8c927f4
Divya. M <divyamahantesh2006@gmail.com>
2026-04-14 18:49:25 +0530

    Create functions_ex1.py

---
+++ functions_ex1.py
@@ -0,0 +1,11 @@
+def area(length, width):
+    return length * width
+
+def perimeter(length, width):
+    return 2 * (length + width)
+
+length = 10
+width = 5
+
+print(f"Area: {area(length, width)}")
+print(f"Perimeter: {perimeter(length, width)}")
