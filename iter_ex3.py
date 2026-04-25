61e135f975f37048bd382d96895784b11b91440a
Divya. M <divyamahantesh2006@gmail.com>
2026-04-25 20:53:27 +0530

    Add Fibonacci generator function and print example

---
+++ iter_ex3.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+print("Fibonacci numbers:")
+for num in fibonacci(8):
+    print(num, end=" ")
