2136c4f790e57f32a4dc4afc545c196cf6ec3a60
Divya. M <divyamahantesh2006@gmail.com>
2026-05-19 14:15:56 +0530

    Add factorial calculation for numbers from 1 to n
    This script calculates and prints the factorial of every number from 1 to n, where n is provided by the user.

---
+++ loops/do-while-loop/for-loop/problem_07.py
@@ -0,0 +1,11 @@
+# Factorial of every number from 1 to n
+
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    fact = 1
+
+    for j in range(1, i + 1):
+        fact *= j
+
+    print("Factorial of", i, "is", fact)
