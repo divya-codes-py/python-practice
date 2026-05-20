5a138f9e82b519a9df6091a906ba421086ff9391
Divya. M <divyamahantesh2006@gmail.com>
2026-05-20 14:42:50 +0530

    Add LCM calculation for two numbers
    Implement LCM calculation using a while loop.

---
+++ loops/do-while-loop/for-loop/problem_15.py
@@ -0,0 +1,15 @@
+# Find LCM of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+largest = max(a, b)
+
+while True:
+    if largest % a == 0 and largest % b == 0:
+        lcm = largest
+        break
+
+    largest += 1
+
+print("LCM is:", lcm)
