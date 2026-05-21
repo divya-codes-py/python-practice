0d56083589df21913183f15f1aa2676fe7a1b5f2
Divya. M <divyamahantesh2006@gmail.com>
2026-05-21 10:11:59 +0530

    Create colour_card.py

---
+++ python-concepts/colour_card.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+colour = input("Enter your favorite colour: ")
+
+print()
+print("---- Your Colour Card ----")
+print(f"Name:   {name}")
+print(f"Age:    {age}")
+print(f"Hi {name}, {colour} is a great colour!")
+Step 5:
