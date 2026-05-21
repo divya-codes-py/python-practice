85db19b26295300fdd6dabd7988b9be237efa833
Divya. M <divyamahantesh2006@gmail.com>
2026-05-21 10:00:49 +0530

    Create string_explorer.py

---
+++ python-concepts/string_explorer.py
@@ -0,0 +1,9 @@
+string_explorer.py
+sentence = input("Enter a sentence: ")
+
+print(f"Original: {sentence}")
+print(f"Uppercase: {sentence.upper()}")
+print(f"Lowercase: {sentence.lower()}")
+print(f"Length: {len(sentence)}")
+print(f"Words count: {len(sentence.split())}")
+print(f"Reversed: {sentence[::-1]}")
