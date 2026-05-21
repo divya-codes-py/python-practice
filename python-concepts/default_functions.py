9f24a5a16b9cac0474d6778e0a420bec1531fa06
Divya. M <divyamahantesh2006@gmail.com>
2026-05-21 10:20:36 +0530

    Create default_functions.py

---
+++ python-concepts/default_functions.py
@@ -0,0 +1,13 @@
+default_functions.py
+def profile(name, city="India", age="Not provided"):
+    print(f"\nHello {name}! 👋")
+    print(f"City: {city}")
+    print(f"Age: {age}")
+
+name = input("Enter your name: ")
+city = input("Enter your city (or press enter): ")
+
+if city == "":
+    profile(name)
+else:
+    profile(name, city)
