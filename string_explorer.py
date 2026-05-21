af675ce2ee877754a1c3c53f06fce6bb83aa1907
Divya. M <divyamahantesh2006@gmail.com>
2026-05-21 09:59:50 +0530

    Delete string_explorer.py

--- string_explorer.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Enter a sentence: ")
-
-print(f"Original: {sentence}")
-print(f"Uppercase: {sentence.upper()}")
-print(f"Lowercase: {sentence.lower()}")
-print(f"Length: {len(sentence)}")
-print(f"Words count: {len(sentence.split())}")
-print(f"Reversed: {sentence[::-1]}")
