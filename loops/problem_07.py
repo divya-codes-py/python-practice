18e78cbf5a99a87bccc37ea856704de8109c8d54
Divya. M <divyamahantesh2006@gmail.com>
2026-05-29 19:01:06 +0530

    Delete loops/problem_07.py

--- loops/problem_07.py
+++
@@ -1,9 +0,0 @@
-# Problem 7: Sum of all even numbers from 1 up to n
-
-n = int(input("Enter a number: "))
-total = 0
-i = 2
-while i <= n:
-    total += i
-    i += 2
-print(f"Sum of even numbers up to {n} = {total}")
