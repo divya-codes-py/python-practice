35f1d4e52655a4af579433900b9dcfad87170038
Divya. M <divyamahantesh2006@gmail.com>
2026-05-29 19:00:55 +0530

    Delete loops/problem_06.py

--- loops/problem_06.py
+++
@@ -1,9 +0,0 @@
-# Problem 6: Sum of first n natural numbers
-
-n = int(input("Enter a number: "))
-total = 0
-i = 1
-while i <= n:
-    total += i
-    i += 1
-print(f"Sum of first {n} natural numbers = {total}")
