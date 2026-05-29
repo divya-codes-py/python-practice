44bc477d18736aede35e6270f4fe6c562938ef0d
Divya. M <divyamahantesh2006@gmail.com>
2026-05-29 19:02:28 +0530

    Delete loops/problem_14.py

--- loops/problem_14.py
+++
@@ -1,9 +0,0 @@
-# Problem 14: Sum of digits of a number
-
-n = int(input("Enter a number: "))
-total = 0
-temp = abs(n)
-while temp > 0:
-    total += temp % 10
-    temp //= 10
-print(f"Sum of digits of {n} = {total}")
